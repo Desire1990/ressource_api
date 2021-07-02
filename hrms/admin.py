b3489746463ab633b0e4f3197dba1739b41e8c00
Desos <desnduwim26@gmail.com>
2021-07-02 14:56:33 +0200

    admin update

--- hrms/admin.py
+++ hrms/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Department)
+admin.site.register(Role)
+admin.site.register(Employee)
+admin.site.register(Attendance)
+admin.site.register(Leave)
+admin.site.register(Recruitment)
+admin.site.register(Bank)
+admin.site.register(Payment)
